aa33783d9fb7dae616934b64b0a3271e43b76a81
吳政穎 <dting31416@gmail.com>
2025-03-19 12:02:33 +0800

    0319

---
+++ 0312/login.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>註冊會員</title>
+        <meta charser="UTF-8">
+    </head>
+    <body>
+        <H1>註冊會員</H1>
+        <form>
+            <p>帳　　號：<input type="text" name="acct"></p>
+            <p>顯示名稱：<input type="text" name="name"></p>
+            <p>密　　碼：<input type="password" name="pass1"></p>
+            <p>確認密碼：<input type="password" name="pass2"></p>
+            <p><input type="submit"></p>
+        </form>
+    </body>
+</html>
--- 0312/reg.php
+++ 0312/reg.php
@@ -2,10 +2,33 @@
     <head>
         <title>註冊會員</title>
         <meta charser="UTF-8">
+        <script>
+<?php
+    if (isset($_POST["acct"])) {
+        if (strcmp($_POST["pass1"], $_POST["pass2"])) {
+            printf("alert('密碼不一致')");
+        } else {
+            $filename = "member.csv";
+            $newmember = true;
+            if (file_exists($filename)) {
+                $fp = fopen($filename, "r");
+                while ($member = fgetcsv($fp, 1000)) {
+                    if (true) {
+
+                    }
+                }
+
+                fclose($fp);
+            }
+        }
+        
+    }
+?>
+        </script>
     </head>
     <body>
         <H1>註冊會員</H1>
-        <form>
+        <form method = "post">
             <p>帳　　號：<input type="text" name="acct"></p>
             <p>顯示名稱：<input type="text" name="name"></p>
             <p>密　　碼：<input type="password" name="pass1"></p>
