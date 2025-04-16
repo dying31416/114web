39872eb67b1c3d3c0877373faa3ca6a63d078345
zheng_yin <dying31416@gmail.com>
2025-04-16 11:41:29 +0800

    有mysql_login

--- msgboard.sql
+++
@@ -1,9 +0,0 @@
-DROP database IF EXISTS msgboard;
-create database msgboard;
-use msgboard;
-DROP TABLE IF EXISTS account;
-CREATE TABLE account(
-    indo INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(64) NOT NULL,
-    pass VARCHAR(256) NOT NULL
-); 
