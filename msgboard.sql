00f120d4c4c92f4ef2f28d6d3e20797738ceaecd
zheng_yin <dying31416@gmail.com>
2025-04-16 11:01:11 +0800

    test

--- msgboard.sql
+++ msgboard.sql
@@ -1,2 +1,9 @@
-create database msg;
-use account
+DROP database IF EXISTS msgboard;
+create database msgboard;
+use msgboard;
+DROP TABLE IF EXISTS account;
+CREATE TABLE account(
+    indo INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(64) NOT NULL,
+    pass VARCHAR(256) NOT NULL
+); 
